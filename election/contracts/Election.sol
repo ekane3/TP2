90edfbda55c0a69e29779c748414a3dd72ce4564
EKANE <ekaneekane3@gmail.com>
2022-09-19 11:24:51 +0200

    [ADD] add candidates

--- election/contracts/Election.sol
+++ election/contracts/Election.sol
@@ -10,4 +10,28 @@ contract Election {
         candidate = "Candidate 1";
     }
 
+    // Model a Candidate
+    struct Candidate{
+        uint id;
+        string name; 
+        uint voteCount;
+    }
+
+    // Read or Write Candidates
+    mapping(uint => Candidate) public candidates;
+
+    // Store Candidates COunt
+    uint public candidatesCount;
+    
+    // function to add candidats to the maping we've created
+    function addCandidate(string _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    // function to add to 2 candidates
+    function Election() public {
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
 }
